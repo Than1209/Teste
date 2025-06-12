7f80806547a953c0de0dea9bd282e177e87d0e94
Thanderson Fábio <thandersonf@gmail.com>
2025-06-12 20:20:12 -0300

    Add files via upload

---
+++ TesteCarro.java
@@ -0,0 +1,14 @@
+package aula1;
+
+public class TesteCarro {
+
+	public TesteCarro() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
