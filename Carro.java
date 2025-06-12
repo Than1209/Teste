9a65a898f2022d898ba432017a98bdf82edc877d
Thanderson Fábio <thandersonf@gmail.com>
2025-06-12 20:19:46 -0300

    Update Carro.java
    Fix: adicionando variavel volts.

--- Carro.java
+++ Carro.java
@@ -1,9 +1,34 @@
-package aula1;
+package com.thanderson.cursojava.aula28.labs;
 
-public class Carro {
-
-	public Carro() {
-		// TODO Auto-generated constructor stub
+public class Lampada {
+	String marca;
+	String cor;
+	boolean ligada;
+	double tensão;
+	String volts;
+	void ligar() {
+		ligada = true;
 	}
-
+	void desligar() {
+		ligada = false;
+	}
+	void mostrarEstado() {
+		
+		if(ligada) {
+			System.out.println("lampada ligada " );
+		}else {
+		System.out.println("lampada desligada");
+	    }
+	}	
+	
+	void mudarEstado() {
+		if(ligada) {
+			desligar();
+			System.out.println("A lampada esta desligada");
+		}else {
+			ligar();
+			System.out.println("A lampada esta ligada");
+		}
+		
+	}
 }
